60b5e961658d50f339974b3982159ecd438ecf4c
the Viper Kailash Suthar <ksuthar937@gmail.com>
2023-04-28 16:36:45 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let popup = document.getElementById("popup");
+let openPopup = () => {
+    popup.classList.add("popup-open");
+}
+let closePopup = () => {
+    popup.classList.remove("popup-open");
+}
